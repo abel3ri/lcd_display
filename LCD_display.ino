ccbfd45328bc3a7d7d64741d156b904f6086f1db
Abel Merete Habteyohannes <abelmerete@yahoo.com>
2024-07-02 22:04:28 +0300

    modify: typo

--- LCD_display.ino
+++ LCD_display.ino
@@ -8,7 +8,7 @@ void setup(){
   lcd.begin(16, 2);
   lcd.print("Hello World!");
   lcd.setCursor(0, 1);
-  lcd.print("https:www.github.com/abel3ri");
+  lcd.print("www.github.com/abel3ri");
 }
 
 void loop(){}
